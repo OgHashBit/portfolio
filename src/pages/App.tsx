e3091dddb9bb4f27bd441652cf3ec0a4220f33f4
OgHashBit <77114028+OgHashBit@users.noreply.github.com>
2022-02-03 21:51:20 +0100

    Update App.tsx

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,7 +1,7 @@
 import Navbar from "../components/Navbar";
 import Introduction from "../components/sections/Introduction";
-import Project-info from "../components/sections/About";
-import About from "../components/sections/AboutMe";
+import Project-info from "../components/sections/AboutMe";
+import Claim from "../components/sections/Claim";
 import Footer from "../components/sections/Footer";
 
 export const App = () => {
