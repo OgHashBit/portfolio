73177946beda1ee28a493a6390d7b9aff8bb7166
OgHashBit <77114028+OgHashBit@users.noreply.github.com>
2022-02-03 20:32:58 +0100

    Update App.tsx

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,7 +1,7 @@
 import Navbar from "../components/Navbar";
 import Introduction from "../components/sections/Introduction";
 import Projects from "../components/sections/Projects";
-import AboutMe from "../components/sections/AboutMe";
+import About from "../components/sections/AboutMe";
 import Footer from "../components/sections/Footer";
 
 export const App = () => {
@@ -9,7 +9,7 @@ export const App = () => {
         <div className="app">
             <Navbar/>
             <Introduction/>
-            <AboutMe/>
+            <About/>
             <Projects/>
             <Footer/>
         </div>
