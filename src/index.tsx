b7a0f390bf7095928175edf1a141094949a1faca
OgHashBit <77114028+OgHashBit@users.noreply.github.com>
2022-02-03 22:16:02 +0100

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
+import React, { useRef, useState } from 'react'
 import ReactDOM from "react-dom";
 import { HashRouter as Router, Route, Routes } from "react-router-dom";
-
+import { Canvas, useFrame } from '@react-three/fiber'
 // PAGES
 import App from "./pages/App";
-
+import Claim from ".pages/Claim
 // CSS
 import "bulma/css/bulma.css";
 import "./styles/index.scss";
@@ -15,4 +16,5 @@ ReactDOM.render(
         </Routes>
     </Router>,
     document.getElementById("root")
+
 );
